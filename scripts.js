d8dc117e42f0095f0b83318907a95e3c32ff4ea1
glorymugizi <glorymugizi@gmail.com>
2023-01-02 22:54:18 +0300

    create and style the mobile version for the home page

---
+++ scripts.js
@@ -0,0 +1,75 @@
+"use strict";
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeBtn = document.querySelector(".close-btn");
+const toggleMenu = document.querySelector(".navbar-toggler-icon");
+
+toggleMenu.addEventListener("click", function () {
+  mobileMenu.classList.remove("hidden");
+});
+closeBtn.addEventListener("click", function () {
+  mobileMenu.classList.add("hidden");
+});
+window.addEventListener("resize", function () {
+  if (window.innerHeight >= 768) {
+    mobileMenu.classList.add("hidden");
+  }
+});
+
+let speakersArray = [
+  {
+    name: "Name",
+    university: "University",
+    img: "./images/speakers/pexels-igreja-dimensão-10295936.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Name",
+    university: "University",
+    img: "./images/speakers/pexels-pranav-digwal-32976.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Name",
+    university: "University",
+    img: "/images/speakers/product-school-DL-yyYDDNX4-unsplash.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Name",
+    university: "University",
+    img: "./images/speakers/pexels-igreja-dimensão-10295936.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Name",
+    university: "University",
+    img: "./images/speakers/product-school-DL-yyYDDNX4-unsplash.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Name",
+    university: "University",
+    img: "./images/speakers/pexels-pranav-digwal-32976.jpg",
+    biodata: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+const featuredSpeakers = document.querySelector(".speakers-container");
+speakersArray.forEach((speaker) => {
+  const { name, university, img, detail, biodata } = speaker;
+  featuredSpeakers.innerHTML += `
+  <div class="speakers-container container">
+    <div class="speakers">
+      <div>
+        <div class="speaker-image">
+          <img
+            src=${img}
+          />
+        </div>
+      </div>
+      <div class="speaker-bio">
+        <h6>${name}</h6>
+        <p class="speaker-education">${university}</p>
+        <p>${biodata}</p>
+      </div>
+    </div></div>`;
+});
